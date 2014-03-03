a134dca27912b816ce1fb07a7b603de0d3b006d2
Thomas Levine <_@thomaslevine.com>
2014-03-03 05:11:52 +0000

    template

--- microblog_web/__init__.py
+++ microblog_web/__init__.py
@@ -1,5 +1,7 @@
 import json
-from bottle import Bottle, request, response
+from bottle import Bottle, request, response, abort, redirect, view
+
+from microblog_web.query import page
 
 messages = {}
 app = Bottle()
@@ -9,4 +11,18 @@ def root():
     response.set_header('Content-Language', 'en')
     response.set_header('Content-Type', 'application/json; charset=utf-8')
     response.status = 200
-    return json.dumps(page(messages, 1))
+    return json.dumps(messages)
+    return json.dumps(page(messages, 0))
+
+@app.get('/<message_id>')
+@view('post')
+def post(message_id):
+    if message_id in messages:
+        datestamp, subject, body = messages[message_id]
+        return {'datestamp':datestamp,'subject':subject,'body':body}
+    else:
+        abort(404, "There's no post with this message-id.")
+
+@app.route('/<message_id>/')
+def slash(message_id):
+    redirect('/' + message_id)
--- microblog_web/query.py
+++ microblog_web/query.py
@@ -1,5 +1,5 @@
 def seq(messages, start = 0, end = None):
-    return reversed(sorted(messages))[start:end]
+    return list(reversed(sorted(messages)))[start:end]
 
 def page(messages, n, step = 10):
     return seq(messages, step * n, step * (n + 1))

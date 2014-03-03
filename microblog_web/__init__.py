5195b4ee102650cb1caba727b09329039389a1cf
Thomas Levine <_@thomaslevine.com>
2014-03-03 05:17:49 +0000

    templates

--- microblog_web/__init__.py
+++ microblog_web/__init__.py
@@ -1,7 +1,8 @@
 import json
-from bottle import Bottle, request, response, abort, redirect, view
+from bottle import Bottle, request, response, abort, redirect, template
 
 from microblog_web.query import page
+import microblog_web.templates as templates
 
 messages = {}
 app = Bottle()
@@ -15,11 +16,11 @@ def root():
     return json.dumps(page(messages, 0))
 
 @app.get('/<message_id>')
-@view('post')
 def post(message_id):
     if message_id in messages:
         datestamp, subject, body = messages[message_id]
-        return {'datestamp':datestamp,'subject':subject,'body':body}
+        params = {'datestamp':datestamp,'subject':subject,'body':body}
+        return template(templates.post, params)
     else:
         abort(404, "There's no post with this message-id.")
 
---
+++ microblog_web/templates.py
@@ -0,0 +1,11 @@
+post = '''<html>
+    {{datestamp}}
+    {{subject}}
+    {{body}}
+</html>'''
+
+root = '''<html>
+    {{datestamp}}
+    {{subject}}
+    {{body}}
+</html>'''

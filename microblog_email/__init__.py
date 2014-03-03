7d489da7563e58db9ea4de15c49aa9bc73576294
Thomas Levine <_@thomaslevine.com>
2014-03-03 06:11:55 +0000

    huh

--- microblog_email/__init__.py
+++ microblog_email/__init__.py
@@ -4,4 +4,4 @@ from microblog_email.parse import parse
 def receive_messages(args, target:dict):
     for email in mailbox(*args):
         message_id, date, subject, body = parse(email)
-        target[message_id] = date, subject, body
+        target[message_id] = message_id, date, subject, body

d195951bd5496c85704018cddfeb31b7a998fb58
Thomas Levine <_@thomaslevine.com>
2014-03-03 02:28:24 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='_',
+    author='Thomas Levine',
+    author_email='_@thomaslevine.com',
+    description='Microblogging',
+    url='https://github.com/tlevine/_.thomaslevine.com.git',
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    packages=['_'],
+    install_requires = [],
+    tests_require = ['nose'],
+    version='0.0.1',
+    license='AGPL'
+)

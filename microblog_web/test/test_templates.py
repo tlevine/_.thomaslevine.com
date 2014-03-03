ec2421be4ea568cd7843c7fd2a79c9e94a9cfc0e
Thomas Levine <_@thomaslevine.com>
2014-03-03 05:58:31 +0000

    test

--- microblog_web/test/test_templates.py
+++ microblog_web/test/test_templates.py
@@ -12,3 +12,12 @@ def test_render_post():
     n.assert_in('Monday, March 03 at 05:27 UTC', result)
     n.assert_in('Subject goes here.', result)
     n.assert_in('Body goes here.', result)
+
+def test_render_root():
+    d = datetime.datetime(2014, 3, 3, 5, 27, 50, 737546)
+    message = (d.timestamp(), 'Subject goes here.', 'Body goes here.')
+    result = templates.render_post([message])
+    #n.assert_in(, result)
+    n.assert_in('Monday, March 03 at 05:27 UTC', result)
+    n.assert_in('Subject goes here.', result)
+    n.assert_in('Body goes here.', result)

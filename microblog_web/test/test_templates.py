673725091d9171ca114a5dc2d415ec69bdbe6398
Thomas Levine <_@thomaslevine.com>
2014-03-03 06:14:16 +0000

    add messag-id

--- microblog_web/test/test_templates.py
+++ microblog_web/test/test_templates.py
@@ -6,8 +6,10 @@ import microblog_web.templates as templates
 
 def test_render_post():
     d = datetime.datetime(2014, 3, 3, 5, 27, 50, 737546)
-    message = (d.timestamp(), 'Subject goes here.', 'Body goes here.')
+    message = ('foobar', d.timestamp(), 'Subject goes here.', 'Body goes here.')
     result = templates.render_post(message)
+
+    n.assert_in('href="/foobar"', result)
     #n.assert_in(, result)
     n.assert_in('Monday, March 03 at 05:27 UTC', result)
     n.assert_in('Subject goes here.', result)
@@ -15,8 +17,10 @@ def test_render_post():
 
 def test_render_root():
     d = datetime.datetime(2014, 3, 3, 5, 27, 50, 737546)
-    message = (d.timestamp(), 'Subject goes here.', 'Body goes here.')
+    message = ('foobar', d.timestamp(), 'Subject goes here.', 'Body goes here.')
     result = templates.render_root([message])
+
+    n.assert_in('href="/foobar"', result)
     #n.assert_in(, result)
     n.assert_in('Monday, March 03 at 05:27 UTC', result)
     n.assert_in('Subject goes here.', result)

dca4ef8ef3958fa58b081857ea0daab749c81e15
Thomas Levine <_@thomaslevine.com>
2014-03-03 05:37:40 +0000

    rename

--- microblog_email/test/test_parse.py
+++ microblog_email/test/test_parse.py
@@ -5,7 +5,7 @@ import nose.tools as n
 from microblog_email.parse import parse
 
 def test_parse():
-    with open(os.path.join('_','test','fixtures','email'), 'rb') as fp:
+    with open(os.path.join('microblog_email','test','fixtures','email'), 'rb') as fp:
         observed = parse(fp.read())
         expected = ('20140303035053.GA18216', 1393818653.0, 'aoeuaoeu', 'oeu\r\n')
         n.assert_tuple_equal(observed, expected)

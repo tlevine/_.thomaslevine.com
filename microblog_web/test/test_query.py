fe00202af2efc067cc3baddbf7c8726c802397bb
Thomas Levine <_@thomaslevine.com>
2014-03-03 05:50:13 +0000

    refactor test

--- microblog_web/test/test_query.py
+++ microblog_web/test/test_query.py
@@ -3,14 +3,14 @@ import nose.tools as n
 import microblog_web.query as query
 
 seq_testcases = [
-    (range(100), 0, 14, reversed(range(86,100))),
-    ('honunhnozzezztzzzuzzzozzzeuthaaa', 0, 3, 'zzz'),
+    (query.seq, reversed(range(86,100)), range(100), 0, 14),
+    (query.seq, 'zzz', 'honunhnozzezztzzzuzzzozzzeuthaaa', 0, 3),
 ]
 
-def check_seq(messages, start, end, expected):
-    observed = query.seq(messages, start, end)
+def check(func, expected, *args):
+    observed = func(*args)
     n.assert_list_equal(list(observed), list(expected))
 
-def test_seq():
-    for messages, start, end, expectation in seq_testcases:
-        yield check_seq, messages, start, end, expectation
+def test():
+    for args in seq_testcases:
+        yield tuple([check] + list(args))

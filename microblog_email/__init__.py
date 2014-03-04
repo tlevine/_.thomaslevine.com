f8f76050aad9eb140c07359a083e7452969382d6
Thomas Levine <_@thomaslevine.com>
2014-03-04 11:05:53 +0000

    fanciness

--- microblog_email/__init__.py
+++ microblog_email/__init__.py
@@ -1,7 +1,40 @@
-from microblog_email.retrieve import mailbox
+from microblog_email.retrieve import mailbox as _mailbox
 from microblog_email.parse import parse
 
-def receive_messages(args, target:dict):
-    for email in mailbox(*args):
-        message_id, date, subject, body = parse(email)
-        target[message_id] = message_id, date, subject, body
+def receive_messages(host:str, address:str, password:str
+                     sending_address:str = None,
+                     forwarding_address:str = None,
+                     target:dict = None,
+                     mailbox = _mailbox):
+    '''
+    Retrieve messages from an IMAP mailbox. Optionally require that messages
+    be sent from a particular addresss and forwarded through a particular address.
+
+    Logging in to the IMAP mailbox:
+
+    :param host: IMAP host (like mail.gandi.net)
+    :param address: Maillbox at that host (like secret@thomaslevine.com)
+    :param password: Password for that mailbox
+
+    Validating that the email came from you:
+
+    :param sending_address: The email address from which emails must be sent
+    :param forwarding_address: The email address through which emails must be forwarded
+
+    Mainly for testing:
+
+    Results:
+
+    :param target: dict into which results should be placed, or None
+
+    If :code:`target` is :code:`None`, results will be yielded.
+    '''
+    for email in mailbox(host, address, password):
+        identifier, date, sent_from, forwarding, subject, body = parse(email)
+        if forwarding_address is not None and forwarding_address in forwarding:
+            if sending_address is not None and sending_address in sent_from:
+                results = identifier, date, subject, body
+                if target is None:
+                    yield results
+                else:
+                    target[identifier] = results

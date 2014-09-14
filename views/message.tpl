0cb492a58b4950a8662f8b04e1e56f2b72887076
Thomas Levine <_@thomaslevine.com>
2014-09-14 19:46:13 +0000

    message

---
+++ views/message.tpl
@@ -0,0 +1,2 @@
+<h1>{{subject}}</h1>
+<article style="white-space: pre;">{{body}}</article>
--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 from notmuch import Database, Query
-from bottle import Bottle, request, response, abort, redirect, view
+from bottle import Bottle, request, response, abort, redirect, view, TEMPLATE_PATH
 
+TEMPLATE_PATH.append('views')
 app = Bottle()
 db = Database()
 
@@ -8,35 +9,46 @@ db = Database()
 def slash(identifier):
     redirect('/' + identifier)
 
-@app.get('/searches/:search')
-def search(search):
-    query = Query(db, query).search_threads()
-    return [[t.get_thread_id(), t.get_subject()] for t in query]
+@app.get('/searches/:querystr')
+@view('flat')
+def search(querystr):
+    query = Query(db, querystr).search_threads()
+    threads = [('/threads/' + t.get_thread_id(), t.get_subject()) \
+               for t in query]
+    return {'messages': threads}
 
 @app.get('/threads/:thread_id')
+@view('flat')
 def thread(thread_id):
     query = Query(db, 'thread:%s' % thread_id)
     if not query.count_threads() == 1:
         return 'Thread not found', 404
-    return [[m.get_message_id(), m.get_subject()] for m in query.search_messages()]
+    messages = [('/messages/' + m.get_message_id(), m.get_header('subject')) \
+                for m in query.search_messages()]
+    return {'messages': messages}
 
 @app.get('/messages/:message_id')
+@view('message')
 def message(message_id):
-    query = Query(db, 'message:%s' % message_id)
+    query = Query(db, 'id:%s' % message_id)
     if not query.count_messages() == 1:
         return 'Message not found', 404
     m = next(query.search_messages())
-    return [m.get_subject(), m.get_thread_id()]
+    print(m)
+    return {
+        'subject': m.get_header('subject'),
+        'body': m.get_part(1),
+    }
 
-@view('flat')
 @app.get('/')
+@view('flat')
 def recent():
     query = Query(db, '')
     messages = []
     for i, m in enumerate(query.search_messages()):
         if i >= 5:
             break
-        messages.append([m.get_message_id(), m.get_header('subject')])
+        messages.append(('/messages/' + m.get_message_id(), m.get_header('subject')))
     return {'messages': messages}
 
 app.run()

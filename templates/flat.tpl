f39de39d741a5d6a6e693090bc7f74394e2653ba
Thomas Levine <_@thomaslevine.com>
2014-09-14 19:31:42 +0000

    template

---
+++ templates/flat.tpl
@@ -0,0 +1,5 @@
+<ul>
+  % for href, description in messages:
+    <li><a href="{{href}}">{{description}}</a></li>
+  % end
+</ul>
--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 from notmuch import Database, Query
-from bottle import Bottle, request, response, abort, redirect
+from bottle import Bottle, request, response, abort, redirect, view
 
 app = Bottle()
 db = Database()
@@ -9,28 +9,34 @@ def slash(identifier):
     redirect('/' + identifier)
 
 @app.get('/searches/:search')
-def search(search)
+def search(search):
     query = Query(db, query).search_threads()
     return [[t.get_thread_id(), t.get_subject()] for t in query]
 
 @app.get('/threads/:thread_id')
 def thread(thread_id):
-    query = notmuch.Query(db, 'thread:%s' % thread_id)
+    query = Query(db, 'thread:%s' % thread_id)
     if not query.count_threads() == 1:
         return 'Thread not found', 404
     return [[m.get_message_id(), m.get_subject()] for m in query.search_messages()]
 
-@app.get('/messages/:message_id'):
+@app.get('/messages/:message_id')
 def message(message_id):
-    query = notmuch.Query(db, 'message:%s' % message_id)
+    query = Query(db, 'message:%s' % message_id)
     if not query.count_messages() == 1:
         return 'Message not found', 404
+    m = next(query.search_messages())
     return [m.get_subject(), m.get_thread_id()]
 
-@app.get('/recent'):
+@view('flat')
+@app.get('/')
 def recent():
-    query = notmuch.Query(db, '')
-    for i, message in enumerate(query.search_messages()):
+    query = Query(db, '')
+    messages = []
+    for i, m in enumerate(query.search_messages()):
         if i >= 5:
             break
-        yield [m.get_message_id(), m.get_subject]
+        messages.append([m.get_message_id(), m.get_header('subject')])
+    return {'messages': messages}
+
+app.run()
